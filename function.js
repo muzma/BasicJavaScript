789e3a64b0f10eba3eb860576eecc11e1765a12b
Mustofa Mahmud <mustofa_mahmud@yahoo.com>
2018-04-24 09:05:45 +0700

    Menggunakan Fungsi

---
+++ function.js
@@ -0,0 +1,12 @@
+// Fungsi 1
+function MyFunction1(){
+    console.log("Ini Fungsi 1");
+}
+MyFunction1();
+
+// Fungsi 2
+function MyFunction2(){
+    return "Ini Fungsi 2";
+}
+
+console.log(MyFunction2());
