47d87ce128bca399cbc5e449907c32522ba199ef
Mustofa Mahmud <mustofa_mahmud@yahoo.com>
2018-04-24 08:52:12 +0700

    Basic Variable

--- variable.js
+++ variable.js
@@ -2,4 +2,4 @@ var nama = "Mustofa Mahmud Abubakar";
 var datasatu = 20;
 var datadua = 30;
 var datatiga = datasatu + datadua;
-console.log("Nama saya: "+ nama + " Hasilnya adalah "+datatiga)
+console.log("Nama saya: "+ nama + " Hasilnya adalah "+datatiga);
