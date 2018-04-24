b4e701e7ff94d1a9bbd1ae9721b79fb02becbd14
Mustofa Mahmud <mustofa_mahmud@yahoo.com>
2018-04-24 08:53:28 +0700

    Menggunakan Switch

---
+++ switch.js
@@ -0,0 +1,15 @@
+var nip = 1234567890;
+
+switch(nip){
+    case 1234567890:
+        console.log("Mustofa");
+        break;
+    case 1234567891:
+        console.log("Mahmud");
+        break;
+    case 1234567892:
+        console.log("Abubakar");
+        break;
+    default:
+        console.log("Gak Kenal...");
+}
